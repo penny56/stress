9d54e9db4e2d9c0a11bad2a317250f367610d6f3
penny56 <penny56@163.com>
2019-12-09 15:36:33 +0800

    Add files via upload
    Add servers.

--- scp.sh
+++ scp.sh
@@ -1,13 +1,20 @@
-# we need to ssh-keygen and ssh-copy-id from the scp originator to the dest before the scp operation
+# we need to ssh-keygen and ssh-copy-id from the scp source to the dest before the scp operation
 # ssh-keygen -b 4096
 # ssh-copy-id 10.20.103.**
 
 declare -A scpDict
 
+# The dict scpDict is for the scp operation peers, the key is source and the value is dest
 scpDict=([T90RHEL11]="10.20.103.61"\
          [T90RHEL12]="10.20.103.62"\
+         [T90RHEL13]="10.20.103.63"\
+         [T90RHEL14]="10.20.103.64"\
+         [T90RHEL15]="10.20.103.65"\
          [T90UBUT11]="10.20.103.81"\
          [T90UBUT12]="10.20.103.82"\
+         [T90UBUT13]="10.20.103.83"\
+         [T90UBUT14]="10.20.103.84"\
+         [T90UBUT15]="10.20.103.85"\
          )
 
 numproc=`ps -ef | grep scp[[:space:]]test | grep -v grep | wc -l`
